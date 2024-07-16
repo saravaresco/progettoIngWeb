3ac5a6a31242294cec833bee57ffc5b27fadfcdc
svevaturola <sveva.turola@edu.unife.it>
2024-07-16 09:44:30 +0200

    Modificato file realizza.java

--- src/main/java/com/parcodivertimenti/parcodivertimenti/model/mo/realizza.java
+++ src/main/java/com/parcodivertimenti/parcodivertimenti/model/mo/realizza.java
@@ -4,12 +4,10 @@ public class realizza {
     private String cf_attore;
     private String nome_spettacolo;
 
-    public realizza(){
 
-    }
     public String getCf_attore(){return cf_attore;}
     public void setCf_attore(String cf_attore){this.cf_attore = cf_attore;}
 
     public String getNome_spettacolo(){return nome_spettacolo;}
-    public void setNome_spettacolo(String descrizione){this.nome_spettacolo = nome_spettacolo;}
+    public void setNome_spettacolo(String nome_spettacolo){this.nome_spettacolo = nome_spettacolo;}
 }
